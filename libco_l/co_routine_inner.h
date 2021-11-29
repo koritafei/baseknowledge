d4a65e1a10fa81e73a40c23277d075127f043e94
koritafei <koritafei@gmail.com>
2021-11-29 18:42:47 +0800

    co routine inner

--- libco_l/co_routine_inner.h
+++ libco_l/co_routine_inner.h
@@ -10,4 +10,15 @@ struct stCoSpec_t {
   void *value;
 };
 
+/**
+ * @brief 共享栈，
+ * 一个进程或线程的地址，是从高位到低位安排数据的，所以stack_bp是栈底，stack_buffer是栈顶
+ * */
+struct stStackMem_t {
+  stCoRoutine_t *occupy_co; // 当前正在使用该共享栈的协程
+  int stack_size;           // 栈的大小
+  char *stack_bp;           // stack_buffer + stack_size 栈底
+  char *stack_buffer;       // 栈的内容，也就是栈顶
+};
+
 #endif /* __CO_ROUTINE_INNER_H__ */
